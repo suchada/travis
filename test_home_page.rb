5c040f0196d0a77ee5b8bccb16f6a58fa712cf36
suchada <su.chavengpoj@gmail.com>
2015-02-05 17:05:31 +0700

    First commit for Travis

---
+++ test_home_page.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+require 'selenium-webdriver'
+
+module Test
+  class TestHomePage < Test::Unit::TestCase
+
+    def setup
+      @driver = Selenium::WebDriver.for :phantomjs
+      @driver.navigate.to('http://yizeng.me')
+    end
+
+    def teardown
+      @driver.quit
+    end
+
+    def test_home_page_title
+      assert_equal('Yi Zeng', @driver.title)
+    end
+  end
+end
